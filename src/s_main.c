8dcf9fad2f03e8d2be28a0cc5c1d3bdb4cb8c5c5
Christof Ressi <christof.ressi@gmx.at>
2020-08-12 23:29:43 +0200

    print warning for "-eventloop" or "-noeventloop" cmdline arg if Pd is compiled without PD_EVENTLOOP

--- src/s_main.c
+++ src/s_main.c
@@ -1203,6 +1203,12 @@ int sys_argparse(int argc, char **argv)
             sys_eventloop = 0;
             argc--; argv++;
         }
+#else
+        else if (!strcmp(*argv, "-eventloop") || !strcmp(*argv, "-noeventloop"))
+        {
+            fprintf(stderr, "Pd compiled without event loop support, ignoring '%s' flag\n", *argv);
+            argc--; argv++;
+        }
 #endif
         else if (!strcmp(*argv, "-send"))
         {
